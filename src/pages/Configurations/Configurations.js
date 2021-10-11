bb8f9a9a106167da6590c0f1e46faacf260f52a1
mtiprojects <mduduzmdluli@gmail.com>
2021-10-11 22:53:06 +0200

    add configuration page

--- src/pages/Configurations/Configurations.js
+++ src/pages/Configurations/Configurations.js
@@ -7,25 +7,92 @@ import { Configurations } from 'components';
 
 const ConfigurationPage = props => {
 	const [key, setKey] = useState('general');
+	const [activeTab, setActiveTab] = useState('referals');
 	const breadcrumb = { heading: "Configurations" };
+	const toggleTab = (e, tab) => {
+		e.preventDefault();
+		setActiveTab(tab);
+	};
+
 	return (
 		<Layout {...props} breadcrumb={breadcrumb}>
 			<Row className="mt-4">
-				<Col lg={8} xl={8}>
-				<Col md={12}>
-				<Tabs id="controlled-tab-example" activeKey={key} onSelect={(k) => setKey(k)} className="mb-3">
-					<Tab eventKey="general" title="General">
-					General
-					</Tab>
-					<Tab eventKey="profile" title="Profile">
-						Profile
-					</Tab>
-					<Tab eventKey="contact" title="Contact">
-						Contact
-					</Tab>
-				</Tabs>
-                </Col>
-				</Col>
+                        <Col md={12} lg={12}>
+                            <Card>
+                                <CardBody>
+                                    <ul className="nav nav-tabs nav-tabs__round mt-0">
+                                        <li className="nav-item">
+                                            <a
+                                                className={`nav-link show ${activeTab === 'referals' ? 'active' : ''}`}
+                                                onClick={e => toggleTab(e, 'referals')}
+                                                data-toggle="tab"
+                                                href="/"
+                                            >
+                                                General
+                                            </a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a
+                                                className={`nav-link show ${activeTab === 'products' ? 'active' : ''}`}
+                                                onClick={e => toggleTab(e, 'products')}
+                                                data-toggle="tab"
+                                                href="/"
+                                            >
+                                                Products
+                                            </a>
+                                        </li>
+										<li className="nav-item">
+                                            <a
+                                                className={`nav-link show ${activeTab === 'transactions' ? 'active' : ''}`}
+                                                onClick={e => toggleTab(e, 'transactions')}
+                                                data-toggle="tab"
+                                                href="/"
+                                            >
+                                                Transactions
+                                            </a>
+                                        </li>
+										<li className="nav-item">
+                                            <a
+                                                className={`nav-link show ${activeTab === 'banking-details' ? 'active' : ''}`}
+                                                onClick={e => toggleTab(e, 'banking-details')}
+                                                data-toggle="tab"
+                                                href="/"
+                                            >
+                                                Banking Details
+                                            </a>
+                                        </li>
+                                    </ul>
+                                    <div className="tab-content">
+                                        <div role="tabpanel" className={`tab-pane show ${activeTab === 'referals' ? 'active' : ''}`}>
+                                            <CardBody className="pl-0 pr-0 pb-0">
+												Tab 1
+                                            </CardBody>
+                                        </div>
+                                        <div role="tabpanel" className={`tab-pane show ${activeTab === 'products' ? 'active' : ''}`}>
+                                            <div className="profile-setting__card">
+                                                <CardBody className="pl-0 pr-0 pb-0">
+                                                    Tab 2
+                                                </CardBody>
+                                            </div>
+                                        </div>
+										<div role="tabpanel" className={`tab-pane show ${activeTab === 'transactions' ? 'active' : ''}`}>
+                                            <div className="profile-setting__card">
+                                                <CardBody className="pl-0 pr-0 pb-0">
+												 Tab 3
+                                                </CardBody>
+                                            </div>
+                                        </div>
+										<div role="tabpanel" className={`tab-pane show ${activeTab === 'banking-details' ? 'active' : ''}`}>
+                                            <div className="profile-setting__card">
+                                                <CardBody className="pl-0 pr-0 pb-0">
+												Tab 4
+                                                </CardBody>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </CardBody>
+                            </Card>
+                        </Col>
 			</Row>
 		</Layout>
 	);
