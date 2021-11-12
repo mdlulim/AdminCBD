2bc78ab98f6990ef61ec96361f62a3067a188d48
Thabiso Ngubane <Thabison@nect2014.local>
2021-11-12 16:59:38 +0200

    realtime save/update page permissions

--- src/pages/UserPermissions/index.js
+++ src/pages/UserPermissions/index.js
@@ -1,28 +1,63 @@
 import React, { useState, useMemo } from 'react';
 import { Card, CardBody, Col, Row } from 'reactstrap';
+import $ from 'jquery';
 import { AuthLayout } from 'containers';
 import { UserPermissions, Common } from 'components';
 import { UserService } from '../../providers';
 import Select from 'react-select';
 import PagePermissionService from 'providers/PagePermissionService';
 
+$(document).ready(function() {
+    $('body').on('click','.check', function(){
+        let val = ($(this).is(':checked') ? true : false);
+        $(this).attr('data-val',val);
+        PagePermissionService.updatePagePermission($(this).attr('data-id'), 
+            {
+                page: $(this).attr('data-content'),
+                low: $('#low'+$(this).attr('data-content')).attr('data-val'),
+                basic: $('#basic'+$(this).attr('data-content')).attr('data-val'),
+                medium: $('#medium'+$(this).attr('data-content')).attr('data-val'),
+                high: $('#high'+$(this).attr('data-content')).attr('data-val'),
+            });
+    })
+});
 const PermissionsList = props => {
     const breadcrumb = { heading: "UserPermissions" };
 
     const [users, setUsers] = useState([]);
-    useMemo(() => {
-        PagePermissionService.getPagePermissions().then((res) => {
-          console.log(res.data.data.results)
-          const userslist = res.data.data.results;
-        setUsers(userslist);
-        });
-    }, []);
+    // const usersList = () => {
+    //     alert(4);
+        // PagePermissionService.updatePagePermission().then((res) => {
+        //   alert(res.data.data.results);
+        //   const userslist = res.data.results;
+
+        //   const myArr = userslist;
+
+        //   myArr.forEach((item, i) => alert(1));
+        // const myList = (
+        // <ul>{myArr.map((item, i) => <li key={item + i}>{item}</li>)}</ul> 
+        // )
+
+        //   
+        // setUsers(userslist);
+        // })};
 
         
-        // const countCompanyAccounts = (type) =>{
-        //     const countTypes = transactions.filter(transaction => transaction.status === type);
-        //     return countTypes.length;
-        // };
+        useMemo(() => {
+            
+            PagePermissionService.getPagePermissions().then((res) => {
+                var arr = [];
+                Object(res.data.data.results).forEach(function(value) {
+                    $(".table > tbody > tr#"+value.page+"").append(
+                    '<td><input id="low'+value.page+'" class="check" data-val="'+value.low+'" data-id="'+value.id+'" data-content="'+value.page+'" type="checkbox" '+(value.low ? 'checked' :'')+'/></td>'+
+                    '<td><input id="low'+value.page+'" class="check" data-val="'+value.basic+'" data-id="'+value.id+'" data-content="'+value.page+'" type="checkbox" '+(value.basic ? 'checked' :'')+'/></td>'+
+                    '<td><input id="low'+value.page+'" class="check" data-val="'+value.medium+'" data-id="'+value.id+'" data-content="'+value.page+'" type="checkbox" '+(value.medium ? 'checked' :'')+'/></td>'+
+                    '<td><input id="low'+value.page+'" class="check" data-val="'+value.high+'" data-id="'+value.id+'" data-content="'+value.page+'" type="checkbox" '+(value.high ? 'checked' :'')+'/></td>');
+                    
+                  });
+            })
+        }, []);
+
 
 	return (
 		<AuthLayout  {...props}
@@ -49,26 +84,18 @@ const PermissionsList = props => {
                                         <thead>
                                             <tr>
                                             <th scope="col">Pages</th>
-                                            <th scope="col">Basic</th>
                                             <th scope="col">Low</th>
+                                            <th scope="col">Basic</th>
                                             <th scope="col">Medium</th>
                                             <th scope="col">High</th>
                                             </tr> 
                                         </thead>
                                         <tbody>
-                                            <tr>
+                                            <tr id="transactions">
                                                 <th scope="row">Transactions</th>
-                                                <td><input type="checkbox" /></td>
-                                                <td><input type="checkbox" /></td>
-                                                <td><input type="checkbox" /></td>
-                                                <td><input type="checkbox" /></td>
                                             </tr>
-                                            <tr>
+                                            <tr id="deposits">
                                                 <th scope="row">Withdrawals</th>
-                                                <td><input type="checkbox" /></td>
-                                                <td><input type="checkbox" /></td>
-                                                <td><input type="checkbox" /></td>
-                                                <td><input type="checkbox" /></td>
                                             </tr>
                                         </tbody>
                                         </table>
@@ -89,26 +116,18 @@ const PermissionsList = props => {
                                         <thead>
                                             <tr>
                                             <th scope="col">Pages</th>
-                                            <th scope="col">Basic</th>
                                             <th scope="col">Low</th>
+                                            <th scope="col">Basic</th>
                                             <th scope="col">Medium</th>
                                             <th scope="col">High</th>
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr>
+                                        <tr id="members">
                                                 <th scope="row">All Members</th>
-                                                <td><input type="checkbox" /></td>
-                                                <td><input type="checkbox" /></td>
-                                                <td><input type="checkbox" /></td>
-                                                <td><input type="checkbox" /></td>
                                             </tr>
-                                            <tr>
+                                            <tr id="wealthcreators">
                                                 <th scope="row">Wealth Creators</th>
-                                                <td><input type="checkbox" /></td>
-                                                <td><input type="checkbox" /></td>
-                                                <td><input type="checkbox" /></td>
-                                                <td><input type="checkbox" /></td>
                                             </tr>
                                         </tbody>
                                         </table>
@@ -134,26 +153,11 @@ const PermissionsList = props => {
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr>
-                                                <th scope="row">All Products</th>
-                                                <td><input type="checkbox" /></td>
-                                                <td><input type="checkbox" /></td>
-                                                <td><input type="checkbox" /></td>
-                                                <td><input type="checkbox" /></td>
+                                        <tr id="products">
+                                                <th scope="row">Products</th>
                                             </tr>
-                                            <tr>
-                                                <th scope="row">Manage Products</th>
-                                                <td><input type="checkbox" /></td>
-                                                <td><input type="checkbox" /></td>
-                                                <td><input type="checkbox" /></td>
-                                                <td><input type="checkbox" /></td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">Buy Products</th>
-                                                <td><input type="checkbox" /></td>
-                                                <td><input type="checkbox" /></td>
-                                                <td><input type="checkbox" /></td>
-                                                <td><input type="checkbox" /></td>
+                                            <tr id="categories">
+                                                <th scope="row">Categories</th>
                                             </tr>
                                         </tbody>
                                         </table>
