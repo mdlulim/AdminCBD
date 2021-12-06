0589471822c640dab7d3009c2e1ef3ee7d80341d
Pelema <abpalema@gmail.com>
2021-12-06 11:44:04 +0200

    Fixed identity docoments view

--- src/components/KYC/levelThree.jsx
+++ src/components/KYC/levelThree.jsx
@@ -12,9 +12,6 @@ export default function Levelhree(props) {
         props.showImage(image);
     }
 
-    console.log(kycApplication, " kyc application")
-
-
     return (
         <Row style={{ marginBottom: "20px", borderBottom: "1px solid gainsboro" }}>
             <Col>
@@ -23,32 +20,34 @@ export default function Levelhree(props) {
                         <h4>Level 3</h4>
                     </Col>
                     <Col md={6}>
-                    <form>
-                       <ButtonGroup size="sm" style={{ display: "flex", justifyContent: "end" }}>
-                            <Button
-                                color="primary"
-                                onClick={()=>{setKycApplication({...kycApplication, status: 'Approved'}); approveLevel({ level: 3, status: true })}}
-                                className={`${kycApplication.status === 'Approved'?'active':''}`}
-                            >
-                                Approve
-                            </Button>
-                            <Button
-                                color="primary"
-                                onClick={()=>{setActiveState(1); approveLevel({ level: 3, status: false, setLevel: setKycApplication, levelData: kycApplication })}}
-                                className={`${kycApplication.status === 'Rejected'?'active':''}`}
-                            >
-                                Decline
-                            </Button>
-                        </ButtonGroup>
-                       </form>
+                        <form>
+                            <ButtonGroup size="sm" style={{ display: "flex", justifyContent: "end" }}>
+                                <Button
+                                    color="primary"
+                                    onClick={() => { setKycApplication({ ...kycApplication, status: 'Approved' }); approveLevel({ level: 3, status: true }) }}
+                                    className={`${kycApplication.status === 'Approved' ? 'active' : ''}`}
+                                >
+                                    Approve
+                                </Button>
+                                <Button
+                                    color="primary"
+                                    onClick={() => { setActiveState(1); approveLevel({ level: 3, status: false, setLevel: setKycApplication, levelData: kycApplication }) }}
+                                    className={`${kycApplication.status === 'Rejected' ? 'active' : ''}`}
+                                >
+                                    Decline
+                                </Button>
+                            </ButtonGroup>
+                        </form>
                     </Col>
                 </Row>
                 <Row>
                     <Col xs={6} md={6} >
                         <div className="form-group">
                             <label>Proof of Address</label>
-                            <Card onClick={() => onShowImage([{ uri: 'https://cdn-cbigold.ams3.digitaloceanspaces.com/'+kycApplication.poa[0] }])}>
-                                <div className="fa fa-file" style={{fontSize: "100px", textAlign: "center", padding: "15px", cursor: "pointer"}}></div>
+                            <Card onClick={() => onShowImage(kycApplication.poa.map(data => {
+                                return { uri: 'https://cdn-cbigold.ams3.digitaloceanspaces.com/' + data }
+                            }))}>
+                                <div className="fa fa-file" style={{ fontSize: "100px", textAlign: "center", padding: "15px", cursor: "pointer" }}></div>
                             </Card>
                         </div>
                     </Col>
