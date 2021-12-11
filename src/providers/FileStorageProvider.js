d896d2e82f0b10954aaa8157c3f85295408e141c
Pelema <abpalema@gmail.com>
2021-12-11 13:16:20 +0200

    Created fileStorageProvider

---
+++ src/providers/FileStorageProvider.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import Config from '../config';
+import SessionProvider from './SessionProvider';
+
+const baseurl =Config.API.BASE_URL_FILE_STORAGE;
+console.log(Config, " ---------")
+const token = SessionProvider.getToken();
+let headers = {
+    'Content-Type': 'application/json'
+};
+
+if (SessionProvider.isValid()) {
+    headers = {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+    }
+}
+
+class FileStorageProvider {
+    static async upload(category, type, file, filename = null) {
+        const form = new FormData();
+        form.append('upload', file);
+        headers['Content-Type'] = file.type;
+        return axios.post(`${baseurl}upload/${category}/${type}${(filename)?`?filename=${filename}`:''}`, form, {
+            headers,
+        })
+            .then((json) => json.data)
+            .then(res => res)
+            .catch((err) => {
+                if (err.response) return err.response.data;
+                return err;
+            });
+    };
+
+    static async filepath(filename) {
+        return await axios({
+            mode: 'no-cors',
+            method: 'GET',
+            url: `${baseurl}file?filename=${filename}`,
+            crossdomain: true,
+            headers,
+        })
+            .then((json) => json.data)
+            .then(res => res)
+            .catch((err) => {
+                if (err.response) return err.response.data;
+                return err;
+            });
+    };
+}
+
+export default FileStorageProvider;
