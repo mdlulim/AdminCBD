fdfb7c143cf3c589409adb9904dd24723c9a664b
Pelema <abpalema@gmail.com>
2021-12-13 13:04:58 +0200

    Added select field for 'withdrawal' and 'deposit' type

--- src/components/Transactions/ImportTransaction.jsx
+++ src/components/Transactions/ImportTransaction.jsx
@@ -5,7 +5,7 @@ import useForm from 'react-hook-form';
 import { FileStorageProvider } from 'providers';
 
 export default function Transactions(props) {
-  const { handleSubmit } = useForm();
+  const { handleSubmit, register } = useForm();
   const [batchFile, setFBatchFile] = useState([])
   const [submitBtn, setSubmitBtn] = useState(false)
 
@@ -24,10 +24,10 @@ export default function Transactions(props) {
     </li>
   ));
 
-  const onSubmit = async () => {
+  const onSubmit = async (form) => {
     setSubmitBtn(true)
     const ext = batchFile[0].type.split('/')[1];
-    const { success, filename } = await FileStorageProvider.upload('batch', 'withdrawal', batchFile[0], Date.now() + '.' + ext);
+    const { success, filename } = await FileStorageProvider.upload('batch', form.type, batchFile[0], Date.now() + '.' + ext);
     setSubmitBtn(false)
     if (!success) {
       alert("Failed! Batch File Fail To Upload")
@@ -41,51 +41,68 @@ export default function Transactions(props) {
     <Card>
       <CardBody>
         <form className="multisteps-form__form mb-8" noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
-          {batchFile.length === 0 &&
-            <CardBody >
-              <div {...getRootProps({ className: 'd-flex flex-column justify-content-center text-center pt-6 pb-5' })}>
-                <input {...getInputProps()} />
-                <i className="fa fa-plus text-secondary mb-3" aria-hidden="true"></i>
-                <h5 className="text-secondary"> Upload File </h5>
-                <p>Drag 'n' drop batch file here, or click to select files</p>
-                <small><em>(Only *.csv are accepted)</em></small><br />
-                <small><em>Delimiter should be comma ","</em></small>
+          <Row>
+            <Col md={6}>
+              <div className="form-group">
+                <label>Transaction Type</label>
+                <select
+                  name="type"
+                  ref={register({ required: true })}
+                  className="form-control"
+                >
+                  <option value="withdrawal">Withdrawal</option>
+                  <option value="deposit">Deposit</option>
+                </select>
               </div>
-            </CardBody>}
-          {batchFile.length > 0 &&
-            <CardBody className="d-flex flex-column justify-content-center text-center pt-6 pb-5">
-              <input {...getInputProps()} />
-              <div className="">
-                <div className="toast-header border-0">
-                  <i className="ni ni-check-bold text-success me-2"></i>
-                  <span
-                    className="me-auto font-weight-bold"
-                    style={{
-                      width: 290,
-                      overflow: 'hidden',
-                      whiteSpace: 'nowrap',
-                      textOverflow: 'ellipsis',
-                      textAlign: 'left'
-                    }}
-                  >
-                    {batchFile[0].name}
-                  </span>
-                  <small className="text-body">{parseInt(batchFile[0].size / 1024)} KB</small>
-                  <i
-                    className="fa fa-trash-o text-lg ms-3 cursor-pointer text-danger"
-                    data-bs-dismiss="toast"
-                    aria-label="Close"
-                    aria-hidden="true"
-                    onClick={() => setFBatchFile([])}
-                  />
-                </div>
-                <hr className="horizontal dark m-0" />
-                <div className="toast-body">
-                  File has been selected, please submit to process the file.
-                </div>
+            </Col>
+            <Col md={6}>
+              {batchFile.length === 0 &&
+                <CardBody >
+                  <div {...getRootProps({ className: 'd-flex flex-column justify-content-center text-center pt-6 pb-5' })}>
+                    <input {...getInputProps()} />
+                    <i className="fa fa-plus text-secondary mb-3" aria-hidden="true"></i>
+                    <h5 className="text-secondary"> Upload File </h5>
+                    <p>Drag 'n' drop batch file here, or click to select files</p>
+                    <small><em>(Only *.csv are accepted)</em></small><br />
+                    <small><em>Delimiter should be comma ","</em></small>
+                  </div>
+                </CardBody>}
+              {batchFile.length > 0 &&
+                <CardBody className="d-flex flex-column justify-content-center text-center pt-6 pb-5">
+                  <input {...getInputProps()} />
+                  <div className="">
+                    <div className="toast-header border-0">
+                      <i className="ni ni-check-bold text-success me-2"></i>
+                      <span
+                        className="me-auto font-weight-bold"
+                        style={{
+                          width: 290,
+                          overflow: 'hidden',
+                          whiteSpace: 'nowrap',
+                          textOverflow: 'ellipsis',
+                          textAlign: 'left'
+                        }}
+                      >
+                        {batchFile[0].name}
+                      </span>
+                      <small className="text-body">{parseInt(batchFile[0].size / 1024)} KB</small>
+                      <i
+                        className="fa fa-trash-o text-lg ms-3 cursor-pointer text-danger"
+                        data-bs-dismiss="toast"
+                        aria-label="Close"
+                        aria-hidden="true"
+                        onClick={() => setFBatchFile([])}
+                      />
+                    </div>
+                    <hr className="horizontal dark m-0" />
+                    <div className="toast-body">
+                      File has been selected, please submit to process the file.
+                    </div>
 
-              </div>
-            </CardBody>}
+                  </div>
+                </CardBody>}
+            </Col>
+          </Row>
           <Col xs={12} className="text-center">
             <hr className="horizontal dark mt-3 mb-1" />
             <div className="button-row mt-4 mb-2">
